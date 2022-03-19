c877fe2e2d1a97380df772824f113bbc230796d2
tasyrikanurul <tasyrikanurul@student.telkomuniversity.ac.id>
2022-03-19 14:24:40 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ Penjumlahan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302201131
+{
+    public class Penjumlahan <T>
+    {
+        public void JumlahTigaAngka(ref T a, ref T b, ref T c)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            dynamic penjumlahan = " ";
+            penjumlahan = 1 + 2 + 3;
+
+            float varFloat = 13 + 02 + 20;
+            Console.WriteLine("Nilai variabel varFloat: {0}", varFloat);
+        }
+
+
+
+    }
+}
+
+    
+
